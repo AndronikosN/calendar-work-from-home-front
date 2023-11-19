778c6c33841c52a6da1c95c060da2b41e93a6303
AndronikosN <129687926+AndronikosN@users.noreply.github.com>
2023-11-19 20:09:18 +0200

    Added navbar with corporate logos

---
+++ src/components/Appbar.vue
@@ -0,0 +1,52 @@
+<template>
+<div>
+    <v-app-bar
+      app
+      color="#6A76AB"
+      dark
+    >
+        <div class="d-flex align-center">
+            <v-img
+            alt="iLink Logo"
+            class="shrink mr-2"
+            contain
+            :src="iLinkImage"
+            transition="scale-transition"
+            width="130"
+            />
+        </div>
+        <v-spacer></v-spacer>
+
+        <v-img
+        alt="Work from home image"
+        class="shrink mt-1"
+        contain
+        :src="WorkImage"
+        width="350"
+        />
+      
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>mdi-logout</v-icon>
+      </v-btn>
+    </v-app-bar>
+</div>
+</template>
+
+<script>
+import iLinkWhite from '../assets/iLinkWhite.png'
+import WorkFromHome from '../assets/WorkFromHome.png'
+
+  export default {
+    name: 'Appbar',
+    data: () => ({
+        iLinkImage: iLinkWhite,
+        WorkImage: WorkFromHome,
+    })
+  }
+</script>
+
+<style scoped>
+
+</style>
