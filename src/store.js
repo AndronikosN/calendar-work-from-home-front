933745fbfd0cbf32a43de89aceabe6d6513da3e2
andronikos <ntinos@ilink.gr>
2023-12-14 17:44:26 +0200

    made some changes to login's css

--- src/store.js
+++ src/store.js
@@ -4,31 +4,40 @@ import Vuex from 'vuex';
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {
-    counter: 0,
-    loggedInUser: {
-      first_name: "Andronikos",
-      last_name: "Ntinos"
+    state: {
+        counter: 0,
+        loggedInUser: {
+            first_name: "Andronikos",
+            last_name: "Ntinos"
+        },
+        properties: {
+            daysPerWeek: 2,
+            daysPerMonth: 5,
+            peoplePerDay: 6,
+        },
+        loggedIn: false,
     },
-    loggedIn:false,
-  },
-  mutations: {
-    setCounter: function(state,data) {
-        state.counter = data
+    mutations: {
+        setCounter: function (state, data) {
+            state.counter = data
+        },
+        setLoggedInUser: function (state, data) {
+            state.loggedInUser = data
+        },
+        setLoggedIn: function (state, data) {
+            state.loggedIn = data
+        },
+        setProperties: function (state, data) {
+            state.properties = data
+        },
     },
-    setLoggedInUser: function (state,data) {
-      state.loggedInUser = data
+    actions: {
+        // Your actions go here
     },
-    setLoggedIn: function (state,data) {
-      state.loggedIn = data
+    getters: {
+        counter: (state) => state.counter,
+        loggedInUser: (state) => state.loggedInUser,
+        loggedIn: (state) => state.loggedIn,
+        properties: (state) => state.properties,
     },
-  },
-  actions: {
-    // Your actions go here
-  },
-  getters: {
-    counter: (state) => state.counter,
-    loggedInUser: (state) => state.loggedInUser,
-    loggedIn: (state) => state.loggedIn,
-  },
 });
